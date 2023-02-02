8a0a7eabc1830e36726475ad3ca638185e3ab0f7
Stanislav Nazar <nazarstanislav.2001@gmail.com>
2023-02-02 17:22:23 +0200

    [BUGFIX] fixed backend module actions return type, added module configuration file for t3 v12

--- Classes/Controller/OpenApiController.php
+++ Classes/Controller/OpenApiController.php
@@ -6,10 +6,13 @@ namespace Mirko\T3customroutes\Controller;
 
 use ReflectionException;
 use TYPO3\CMS\Core\Site\SiteFinder;
+use TYPO3\CMS\Core\Http\HtmlResponse;
+use TYPO3\CMS\Core\Http\JsonResponse;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use Mirko\T3customroutes\Service\OpenApiBuilder;
 use TYPO3\CMS\Core\Exception\SiteNotFoundException;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
+use TYPO3\CMS\Core\Cache\Exception\NoSuchCacheException;
 use Mirko\T3customroutes\Provider\RoutesLoader\RouteProvider;
 use GoldSpecDigital\ObjectOrientedOAS\Exceptions\InvalidArgumentException as OasInvalidArgumentException;
 
@@ -18,7 +21,7 @@ class OpenApiController extends ActionController
     /**
      * @throws SiteNotFoundException
      */
-    public function displayAction(): void
+    public function displayAction(): HtmlResponse
     {
         $pageId = $this->request->getAttribute('site')->getRootPageId();
         $siteFinder = new SiteFinder();
@@ -40,26 +43,27 @@ class OpenApiController extends ActionController
             GeneralUtility::makeInstance(SiteFinder::class)
                 ->getSiteByIdentifier($siteIdentifier->getIdentifier())
         );
+
+        return new HtmlResponse($this->view->render());
     }
 
     /**
      * @param string $siteIdentifier
-     * @return string
+     * @return JsonResponse
      * @throws OasInvalidArgumentException
      * @throws ReflectionException
-     * @throws SiteNotFoundException
+     * @throws SiteNotFoundException|NoSuchCacheException
      */
-    public function specAction(string $siteIdentifier): string
+    public function specAction(string $siteIdentifier): JsonResponse
     {
         $originalRequest = $GLOBALS['TYPO3_REQUEST'];
         $site = GeneralUtility::makeInstance(SiteFinder::class)
             ->getSiteByIdentifier($siteIdentifier);
         $imitateSiteRequest = $originalRequest->withAttribute('site', $site);
         $GLOBALS['TYPO3_REQUEST'] = $imitateSiteRequest;
-        $output = OpenApiBuilder::build(RouteProvider::getConfiguredRoutes()->all())
-            ->toJson();
+        $output = OpenApiBuilder::build(RouteProvider::getConfiguredRoutes()->all());
         $GLOBALS['TYPO3_REQUEST'] = $originalRequest;
 
-        return $output;
+        return new JsonResponse($output->toArray());
     }
 }
